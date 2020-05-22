c7b406537a5676bba668760da0f7946f16713fde
Leonel Sanches da Silva <leonel@designliquido.com.br>
2020-05-22 11:07:46 -0500

    Implementing Monolog with the routes.

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 // Closure route example
 
 $router->get('/', function ($request, $response) {
@@ -11,4 +14,13 @@ $router->get('/', function ($request, $response) {
 $router->group(['namespace' => 'App\\Controllers\\'], function ($router) {
     // Controller controller example
     $router->get('/about', 'HomeController::actionAboutGet');
+});
+
+$router->get('/my-route', function ($request, $response) {
+    $log = new Logger('name');
+    $log->pushHandler(new StreamHandler('php://stdout', Logger::WARNING));
+    
+    $log->warning('Foo');
+    $log->error('Bar');
+    return $response->create('Hello World!');
 });
